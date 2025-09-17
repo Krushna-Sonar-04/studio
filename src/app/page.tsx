7ce05f7b95fa8682f6abbc5714b78a0f6896e34a
Krushna Sonar <kasonar0111@gmail.com>
2025-09-17 17:31:01 +0000

    i cannot even scroll landing page  .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function LandingPage() {
   ];
 
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="bg-background">
       <main className="flex-1">
         {/* Hero Section */}
         <section className="w-full py-24 md:py-32 lg:py-48">
